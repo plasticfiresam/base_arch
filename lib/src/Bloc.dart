aefd874f5be9ce642ab9a3f4bcc8bb061fb4f9a3
Dmitriy Orteney <domovoy1905@gmail.com>
2020-11-02 18:27:21 +0700

    privated setter

--- lib/src/Bloc.dart
+++ lib/src/Bloc.dart
@@ -8,28 +8,29 @@ abstract class Bloc<S extends State, A extends Action, R extends Reducer> {
   final _stateController = StreamController<S>.broadcast();
   final _actionsController = StreamController<A>();
 
-  S currentState;
+  S _currentState;
 
   Stream<State> get state => _stateController.stream;
+  S get currentState => _currentState;
   Sink<Action> get action => _actionsController.sink;
   StateListener _stateListener;
 
   final Map<Type, R> _reducers;
 
   Bloc(this._reducers, S initialState) {
-    currentState = initialState;
+    _currentState = initialState;
     _actionsController.stream.listen(handleAction);
     _stateListener = (state) {
       _stateController.sink.add(state);
-      currentState = state;
+      _currentState = state;
     };
 
-    _stateController.add(currentState);
+    _stateController.add(_currentState);
   }
 
   void handleAction(A action) async {
     _reducers[action.runtimeType]
-        ?.call(currentState, action)
+        ?.call(_currentState, action)
         ?.listen(_stateListener);
   }
 
